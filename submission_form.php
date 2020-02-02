af192593d8d6bc5813c8e816f6e7f284d8af595b
itrebecca <negron.rebecca@gmail.com>
2020-02-01 21:44:20 -0800

    changes to index and php

--- submission_form.php
+++ submission_form.php
@@ -1,8 +1,20 @@
-<html>
-<body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-Welcome <?php echo $_POST["firstName"]; ?><br>
-Your email address is: <?php echo $_POST["lastname"]; ?>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
+    <title>Women in Digital</title>
+   </head>
+ <body>
+<div class="container">
+Thank you <?php echo $_POST["firstName"]; ?> <?php echo $_POST["lastName"]; ?>
+<br>
+For your pledge of $<?php echo $_POST["amount"]; ?>
+</div>
 </body>
 </html>
